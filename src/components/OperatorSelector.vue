65375249e18c390afa1e2bbcae668f6d4439548e
Adish Parekh <Adish@Adish-Mac-Mini.local>
2020-09-16 19:34:10 +0530

    Add all my files

---
+++ src/components/OperatorSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="$emit('changeOperator', '+')">Addition</button>
+    <button @click="$emit('changeOperator', '-')">Subtraction</button>
+    <button @click="$emit('changeOperator', '/')">Division</button>
+    <button @click="$emit('changeOperator', '*')">Multiplication</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
